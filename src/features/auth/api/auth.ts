ed5d723ab562e8f246f941a537afe53288f259e0
Nikita <nikitavidnov@yandex.ru>
2025-12-08 20:42:14 +0700

    master - fix api

--- src/features/auth/api/auth.ts
+++ src/features/auth/api/auth.ts
@@ -3,29 +3,30 @@ import type {
   IPayloadRegister,
   IPayloadVerificationCode,
 } from "~/entities/user/types";
+import { useApi } from "~/shared/composable/useApi";
 
 export function register(payload: IPayloadRegister) {
-  const { $api } = useNuxtApp();
+  const api = useApi();
 
-  return $api("/api/users/create", {
+  return api("/api/users/create", {
     method: "POST",
     body: payload,
   });
 }
 
 export function sendVerificationCode(payload: IPayloadVerificationCode) {
-  const { $api } = useNuxtApp();
+  const api = useApi();
 
-  return $api("/verification/send", {
+  return api("/verification/send", {
     method: "POST",
     body: payload,
   });
 }
 
 export function checkVerificationCode(payload: IPayloadCheckVerificationCode) {
-  const { $api } = useNuxtApp();
+  const api = useApi();
 
-  return $api("/verification/check", {
+  return api("/verification/check", {
     method: "POST",
     body: payload,
   });
